a86071a3ac7c8a76add30f8c7af3dd34d3f1399a
Calvin Josenhans <60663079+HalflingHelper@users.noreply.github.com>
2023-03-24 18:31:01 -0400

    Chapter 22 Complete

--- src/vm.c
+++ src/vm.c
@@ -148,6 +148,16 @@ static InterpretResult run()
             push(NUMBER_VAL(-AS_NUMBER(pop()))); 
             break;
         case OP_POP: pop(); break;
+        case OP_GET_LOCAL: {
+            uint8_t slot = READ_BYTE();
+            push(vm.stack[slot]);
+            break;
+        }
+        case OP_SET_LOCAL: {
+            uint8_t slot = READ_BYTE();
+            vm.stack[slot] = peek(0);
+            break;
+        }
         case OP_DEFINE_GLOBAL: {
             ObjString* name = READ_STRING();
             tableSet(&vm.globals, name, peek(0));
