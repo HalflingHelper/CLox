1505b0761290b3a7a65972de17104ae2a1ebad36
Calvin Josenhans <60663079+HalflingHelper@users.noreply.github.com>
2023-05-10 15:09:24 -0400

    Chapter 28

--- src/debug.c
+++ src/debug.c
@@ -20,6 +20,15 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
     return offset + 2;
 }
 
+static int invokeInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    uint8_t argCount = chunk->code[offset + 2];
+    printf("%-16s (%d args) %4d '", name, argCount, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 3;
+}
+
 static int constantLongInstruction(const char* name, Chunk* chunk, int offset) {
     uint32_t constant = 0; // The index of the constant
     constant += chunk->code[offset + 1];
@@ -118,6 +127,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return jumpInstruction("OP_LOOP", -1, chunk, offset);
         case OP_CALL:
             return byteInstruction("OP_CALL", chunk, offset);
+        case OP_INVOKE:
+            return invokeInstruction("OP_INVOKE", chunk, offset);
         case OP_CLOSURE: {
             offset++;
             uint8_t constant = chunk->code[offset++];
